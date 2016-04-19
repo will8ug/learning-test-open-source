1111318bafa3c6622de5d45264fadaa6a759e542
Will Yuan <zhijun.yuan@ericsson.com>
2016-04-19 11:11:47 +0800

    More learning test on MutableDateTime.

--- OpenSourceStudy/src/test/java/net/will/opensourcestudy/jodatime/MutableDateTimeTest.java
+++ OpenSourceStudy/src/test/java/net/will/opensourcestudy/jodatime/MutableDateTimeTest.java
@@ -2,6 +2,7 @@ package net.will.opensourcestudy.jodatime;
 
 import static org.junit.Assert.*;
 
+import org.joda.time.DateTimeZone;
 import org.joda.time.MutableDateTime;
 import org.junit.Test;
 
@@ -50,6 +51,38 @@ public class MutableDateTimeTest {
         
         MutableDateTime mdt2 = new MutableDateTime(2016, 3, 15, 4, 3, 2, 1);
         assertTrue(mdt1.equals(mdt2));
+    }
+
+    @Test
+    public void testConstructor_Args_Object() {
+        MutableDateTime mdt1 = new MutableDateTime("1970-01-01T00:00:00");
+        
+        MutableDateTime mdt2 = new MutableDateTime(1970, 1, 1, 0, 0, 0, 0);
+        assertTrue(mdt1.equals(mdt2));
+    }
+
+    @Test
+    public void testConstructor_Args_Long_Zero_DateTimeZone() {
+        MutableDateTime mdt1 = new MutableDateTime(0, DateTimeZone.UTC);
+        
+        MutableDateTime mdt2 = new MutableDateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeZone.UTC);
+        assertTrue(mdt1.equals(mdt2));
+    }
+
+    @Test
+    public void testConstructor_Args_Long_Zero() {
+        MutableDateTime mdt1 = new MutableDateTime(0);
+        MutableDateTime mdt2 = new MutableDateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeZone.UTC);
+        assertEquals(mdt1.getMillis(), mdt2.getMillis());
+    }
+
+    @Test
+    public void testEquals() {
+        MutableDateTime mdt1 = new MutableDateTime(0);
+        MutableDateTime mdt2 = new MutableDateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeZone.UTC);
+        
+        // the milliseconds are the same, but "equals()" returns false:
+        assertFalse(mdt1.equals(mdt2));
     }
 
 }
